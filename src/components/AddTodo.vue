db263c8fd3c54edeaec86fbb5feb330222058c1a
David Ahmed <david4ahmed@gmail.com>
2019-04-13 10:27:47 -0400

    commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <!-- v-model needed for bindings for what's typed on form input and textarea with this -->
+      <input type="text" v-model="title" name="title" placeholder="AddTodo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from "uuid";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false
+      };
+      // Send up to parent
+      this.$emit("add-todo", newTodo);
+
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
+
+
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Binding a CSS class and adding is_complete if todo.completed is true -->
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <!-- @click and v-on both do event handlers -->
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
+
